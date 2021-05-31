708d730ad9bea6f7425e68e91cd4d153a6559e65
Philip Tu <tuyuchen77@berkeley.edu>
2021-05-31 00:04:01 -0700

    clean up pylmps.py

--- lib/pylmps.py
+++ lib/pylmps.py
@@ -1,20 +1,34 @@
 #!/usr/bin/env python
 
+#==================================================#
+#| Python module written for lammps-related tasks.|#
+#==================================================#
+
+
 import re
 import os
 import json
 import pandas as pd
 
+
 def change_var_value(input_file, var, new_val):
-	""" Changes the corresponding value of a variable in a lammps .in file.
+	""" Change the value of a variable in a lammps .in file.
 
-	Args:
-		input_file (str): path to lammps .in file to be modified from the working directory "jupyter"
-		var (str): name of the variable whose value is to be modified
-		new_val (float): the new value of var
+	Parameters
+	----------
+	input_file : str
+		Path to lammps .in file that will be modified from the current working directory.
 
-	Returns:
-		NULL
+	var : str
+		Name of the variable whose value is to be modified.
+
+	new_val : float
+		The new value of var.
+
+
+	Returns
+	-------
+		None
 	"""
 	file = open(input_file, 'r')
 	lines = file.readlines()
@@ -35,16 +49,27 @@ def change_var_value(input_file, var, new_val):
 	file.close()
 
 
-def run_sim(file_name, sim_dir, lmp_path):
-	""" Run a lammps simulation by executing a lammps .in file.
+def run_lmp(file_name, sim_dir, lmp_path):
+	""" Execute a lammps .in file locally with mpirun enabled.
 
-	Args:
-		file_name (str): name (only) of the .in file to be run.
-		sim_dir (str): path from working directory of "jupyter" to directory in which the .in file is located.
-		lmp_path (str): path to the lmp executable.
+	A total of 4 cores is assumed for the local CPU.
 
-	Returns:
-		NULL
+	Parameters
+	----------
+	file_name : str
+		Name (only) of the .in file to be run.
+
+	sim_dir : str
+		(Relative) path from the current working directory to the directory in
+		which the .in file is located.
+		
+	lmp_path : str
+		Path to the lmp executable.
+
+
+	Returns
+	-------
+		None
 	"""
 	cwd = os.getcwd()
 	sim_dir = cwd + '/' + sim_dir
@@ -54,21 +79,30 @@ def run_sim(file_name, sim_dir, lmp_path):
 
 
 def read_json_to_dict(file_path, params=True):
-	""" Reads a JSON file to Python dictionary.
+	""" Read a JSON file to Python dictionary.
 	
-	Args:
-		file_path (str): absolute/relative path to the .json file.
-		params (bool): whether or not the parsed file is params.json.
-			If True: the file is structured like a simple Python dict.
-			If False: the file encodes a pandas DataFrame and is most
-				likely a product of calling pylmps.str_array_dict_to_json
-				or pandas.DataFrame.to_json.
-			
-			Defaults to True.
+	Parameters
+	----------
+	file_path : str
+		Absolute/relative path to the .json file.
+		
+	params : bool, optional
+		Indicate whether or not the parsed file is params.json.
+
+		If True: the file is structured like a simple Python dict.
+		If False: the file is assumed to encode a pandas DataFrame and is most
+			likely a product of calling pylmps.str_array_dict_to_json
+			or pandas.DataFrame.to_json.
+		
+		Defaults to True.
 	
-	Returns:
-		dict: Python dictionary equivalent of the json file with format:
-			<variable> (str): <value> (float or numpy array)
+
+	Returns
+	-------
+	dict
+		Python dictionary equivalent of the json file with format:
+			<variable> (str): <value> (float if params=True or numpy array
+			if otherwise)
 	"""
 	if params:
 		with open(file_path, "r") as file:
@@ -85,17 +119,27 @@ def str_array_dict_to_json(str_arr_dict, to_file_path):
 	as a .json file.
 	
 	All values (numpy arrays) of the dictionary are assumed to have the same size
-	as each array represents one column in the underlying dataframe encoded by the
+	as each array represents one column in the corresponding dataframe encoded by the
 	passed-in Python dictionary.
 	
-	Args:
-		str_arr_dict (dict): data stored in the form of a Python dictionary with
-			the key-value pairs structured as followed:
-				<variable/column name> (str): <values/entries> (1-D numpy array)
-		to_file_path (str): absolute/relative path to the file where the Python
-			dictionary will be written to disk in JSON format. Should end with ".json".
-	Returns:
-		None: output written to file.
+	Parameters
+	----------
+	str_arr_dict : dict 
+		Data stored in the form of a Python dictionary with the key-value pairs
+		structured as followed:
+			<variable/column name> (str): <values/entries> (1-D numpy array)
+	
+	to_file_path : str
+		Absolute/relative path to the file where the Python dictionary will be written
+		to disk in JSON format.
+		
+		Should end with ".json".
+		
+	
+	Returns
+	-------
+	None
+		Output written to file.
 	"""
 	df = pd.DataFrame.from_dict(str_arr_dict)
 	df.to_json(to_file_path)
